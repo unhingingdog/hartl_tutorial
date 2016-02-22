43e1e7d3be718648e1a7fc0a33ef99e07f9f0460
HAMISH <constatcopywriting@gmail.com>
2016-02-22 20:02:41 +1300

    added integration test for  static pages and Sign up controller, template and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get  'about'   => 'static_pages#about'
   get  'contact' => 'static_pages#contact'
   get  'help'    => 'static_pages#help'
+  get  'signup'  => 'users#new'
 end
