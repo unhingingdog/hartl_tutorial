5eb95eff88a575c8480f8df187ed35bae9fb64ff
HAMISH <constatcopywriting@gmail.com>
2016-03-28 22:58:09 +1300

    index page with tests

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'login'          =>  'sessions#new'
   post 'login'        =>  'sessions#create'
   delete 'logout' =>  'sessions#destroy'
+  get 'index' => 'users#index'
   resources :users
 end
